bf1d360e69e18a34757710231a9f70f03abefc77
Chany <inshakr2@naver.com>
2020-08-15 18:55:15 +0900

    Update

---
+++ opentutorials/src/IoTInput.java
@@ -0,0 +1,31 @@
+import org.opentutorials.iot.*;
+import javax.swing.*;
+
+public class IoTInput {
+    public static void main(String[] args) {
+
+        String My_ID = JOptionPane.showInputDialog("Input Your Addr :");
+        String bright = JOptionPane.showInputDialog("Set Your Lamp Bright :");
+
+        // Elevator call
+        Elevator myElevator = new Elevator(My_ID);
+        myElevator.callForUp(1);
+
+        // Security off
+        Security mySecurity = new Security(My_ID);
+        mySecurity.off();
+
+        // Light on
+        Lighting hallLamp = new Lighting(My_ID + " / Hall Lamp");
+        hallLamp.on();
+
+        Lighting floorLamp = new Lighting(My_ID + " / floorLamp");
+        floorLamp.on();
+
+        DimmingLights moodLamp = new DimmingLights(My_ID + "moodLamp");
+        moodLamp.setBright(Double.parseDouble(bright));
+        moodLamp.on();
+
+
+    }
+}
