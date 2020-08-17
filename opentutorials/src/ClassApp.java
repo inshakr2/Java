c91188f47bb6f9295cb14a330695ca3ca23dac34
Chany <inshakr2@naver.com>
2020-08-17 22:48:11 +0900

    Update

---
+++ opentutorials/src/ClassApp.java
@@ -0,0 +1,11 @@
+public class ClassApp {
+    public static void main(String[] args) {
+
+        System.out.println(Math.PI);
+        System.out.println(Math.floor(Math.PI));
+        System.out.println(Math.ceil(Math.PI));
+
+
+
+    }
+}
