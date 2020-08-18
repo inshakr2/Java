a1809de61cb87d05d68d2917186cf24e011fe35b
Chany <inshakr2@naver.com>
2020-08-18 21:00:18 +0900

    Update

--- opentutorials/MyApp/AccountingApp.java
+++ opentutorials/MyApp/AccountingApp.java
@@ -1,14 +1,25 @@
 public class AccountingApp {
     public static void main(String[] args) {
 
-        System.out.println("Value of supply : " + 10000.0);
-        System.out.println("VAT : " + (10000.0 * 0.1));
-        System.out.println("Total : " + (10000.0 + 10000.0 * 0.1));
-        System.out.println("Expense : " + (10000.0 * 0.3));
-        System.out.println("Income : " + (10000.0 - 10000.0 * 0.3));
+        double ValueOfSupply = Double.parseDouble(args[0]);
+        double VatRate = 0.1;
+        double expenseRate = 0.3;
+        double Vat = ValueOfSupply * VatRate;
+        double Total = ValueOfSupply + Vat;
+        double Expense = ValueOfSupply * expenseRate;
+        double Income = ValueOfSupply - Expense;
+        double Dividend1 = Income * 0.5;
+        double Dividend2 = Income * 0.2;
+        double Dividend3 = Income * 0.3;
 
-        System.out.println("Dividend 1 : " + (10000.0 - 10000.0 * 0.3) * 0.5);
-        System.out.println("Dividend 2 : " + (10000.0 - 10000.0 * 0.3) * 0.2);
-        System.out.println("Dividend 3 : " + (10000.0 - 10000.0 * 0.3) * 0.3);
+        System.out.println("Value of supply : " + ValueOfSupply);
+        System.out.println("VAT : " + Vat);
+        System.out.println("Total : " + Total);
+        System.out.println("Expense : " + Expense);
+        System.out.println("Income : " + Income);
+
+        System.out.println("Dividend 1 : " + Dividend1);
+        System.out.println("Dividend 2 : " + Dividend2);
+        System.out.println("Dividend 3 : " + Dividend3);
     }
 }
