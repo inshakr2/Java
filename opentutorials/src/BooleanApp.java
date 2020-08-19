1a082d00900c30d92893e0e2fcb2c53022ad7f92
Chany <inshakr2@naver.com>
2020-08-19 23:13:55 +0900

    Update

---
+++ opentutorials/src/BooleanApp.java
@@ -0,0 +1,20 @@
+public class BooleanApp {
+    public static void main(String[] args) {
+
+        boolean a = true;
+        boolean b = false;
+
+        System.out.println(a && b);
+        System.out.println(a | b);
+
+        String c = "Hello Chany!";
+        System.out.println(c.contains("Chany"));
+        System.out.println(c.contains("World!"));
+
+        System.out.println(1 > 1);
+        System.out.println(1 == 1);
+        System.out.println(1 >= 1);
+        System.out.println(1 <= 1);
+
+    }
+}
