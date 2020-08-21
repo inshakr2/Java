8395c64839ba7114bdb815e58ee4e5883718c6f7
Chany <inshakr2@naver.com>
2020-08-21 23:22:25 +0900

    Update

---
+++ opentutorials/src/AuthApp.java
@@ -0,0 +1,16 @@
+public class AuthApp {
+    public static void main(String[] args) {
+
+        String id = "Chany";
+        String inputID = "Chany";
+
+        System.out.println("Hi");
+
+        if (id.equals(inputID)){
+            System.out.println("Master!");
+        } else {
+            System.out.println("Who are you?");
+        }
+
+    }
+}
