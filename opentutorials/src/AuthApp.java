0633a2033ba0341419c790a148d654814c2e9256
Chany <inshakr2@naver.com>
2020-08-23 23:55:05 +0900

    Update

--- opentutorials/src/AuthApp.java
+++ opentutorials/src/AuthApp.java
@@ -2,15 +2,17 @@ public class AuthApp {
     public static void main(String[] args) {
 
         String id = "Chany";
-        String inputID = "Chany";
+        String inputID = args[0];
+
+        String pass = "1111";
+        String inputPass = args[1];
 
         System.out.println("Hi");
 
-        if (id.equals(inputID)){
-            System.out.println("Master!");
+        if (id.equals(inputID) && pass.equals(inputPass)) {
+            System.out.println("Master !");
         } else {
-            System.out.println("Who are you?");
+            System.out.println("Who Are You?");
         }
-
     }
 }
