8e8a193aefc74eb81ab4cb6b628b1fa0f999a61e
Chany <inshakr2@naver.com>
2020-12-15 20:43:35 +0900

    Lecture interface ... utilize case

---
+++ Study/src/main/java/oop/multi1/Car.java
@@ -0,0 +1,8 @@
+package main.java.oop.multi1;
+
+// interface의 활용 2
+// - 추상 메소드 설정용
+public interface Car {
+    void drive();
+    void horn();
+}
---
+++ Study/src/main/java/oop/multi1/RSP.java
@@ -0,0 +1,10 @@
+package main.java.oop.multi1;
+
+// interface의 활용 1
+// - 상속과 무관하게 상수만 저장해서 사용
+public interface RSP {
+    int 가위 = 1;
+    int 바위 = 2;
+    int 보 = 3;
+
+}
