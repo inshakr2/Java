c46d6d94f6f69710d3fa164cdb78140b1434d727
Chany <inshakr2@naver.com>
2020-12-16 20:36:35 +0900

    New Chapter - API

---
+++ Study/src/main/api/lang/object/Test01.java
@@ -0,0 +1,13 @@
+package main.api.lang.object;
+
+//import java.lang.*;
+
+public class Test01 {
+    public static void main(String[] args) {
+
+        // Object Class 객체 생성
+        Object a = new Object();
+
+
+    }
+}
