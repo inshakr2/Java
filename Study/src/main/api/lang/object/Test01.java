3f13ea141077b399523abc4d220d065cbcde1a84
Chany <inshakr2@naver.com>
2020-12-16 20:47:54 +0900

    Tutorial lang.object

--- Study/src/main/api/lang/object/Test01.java
+++ Study/src/main/api/lang/object/Test01.java
@@ -7,7 +7,19 @@ public class Test01 {
 
         // Object Class 객체 생성
         Object a = new Object();
+        Object b = new Object();
 
+        int v1 = a.hashCode();
+        int v2 = b.hashCode();
+
+        System.out.println(v1);
+        System.out.println(v2);
+
+        String s1 = a.toString();
+        String s2 = b.toString();
+
+        System.out.println(s1);
+        System.out.println(s2);
 
     }
 }
