ebc65d4aff51e4568fedce21560d107feefb973c
Chany <inshakr2@naver.com>
2020-12-22 21:50:15 +0900

    stringBuffer - Reason Why need it

--- Study/src/main/api/lang/stringbuffer/Test01.java
+++ Study/src/main/api/lang/stringbuffer/Test01.java
@@ -10,6 +10,14 @@ public class Test01 {
             str += "Hello";
             str += "\n";
         }
-        // String의 덧셈 구조적 문제, 메모리에 계속 올리는 구조
+        // String의 덧셈 구조적 문제, 메모리에 계속 올리는 구조 - 매우 오래걸림 & 낭비
+
+        StringBuffer buffer = new StringBuffer();
+
+        for (int i = 0; i < 10000; i++) {
+            buffer.append("Hello");
+            buffer.append("\n");
+
+        }
     }
 }
