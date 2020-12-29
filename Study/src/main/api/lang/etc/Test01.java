3beedf0a94f36f96019d93891d32ba18755d2f3c
Chany <inshakr2@naver.com>
2020-12-29 23:37:43 +0900

    Math Class Lecture - 2 Method

--- Study/src/main/api/lang/etc/Test01.java
+++ Study/src/main/api/lang/etc/Test01.java
@@ -9,6 +9,24 @@ public class Test01 {
 
         // abs() : 절대값
         System.out.println(Math.abs(-1));
+
+        // round() : 반올림
+        System.out.println(Math.round(1.5));
+
+        // pi
+        System.out.println(Math.PI);
+
+        // random : 무작위 실수 0이상 1미만
+        System.out.println(Math.random());
+
+        // min & max
+        int x = Math.min(10, 15);
+        int y = Math.max(10, 15);
+        System.out.println(x);
+        System.out.println(y);
+
+        // pow() : 거듭제곱
+        System.out.println(Math.pow(2, 10));
     }
 
 }
