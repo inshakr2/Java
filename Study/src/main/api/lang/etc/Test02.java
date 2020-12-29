a16d3723c7744e6439a764b2ba12ba8b6b16c396
Chany <inshakr2@naver.com>
2020-12-29 23:43:58 +0900

    System Class Lecture - 1

---
+++ Study/src/main/api/lang/etc/Test02.java
@@ -0,0 +1,12 @@
+package main.api.lang.etc;
+
+public class Test02 {
+    public static void main(String[] args) {
+
+        // java.lang.System
+        //  - 생성자 X
+        //      : Math와 동일
+        // Program 밖의 어떤 도구에 접근할 수 있는 or OS를 제어할 수 있는 기능을 가지고 있음.
+
+    }
+}
