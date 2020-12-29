b9878b8a3519e40b24ec839e96f0315470afc0a1
Chany <inshakr2@naver.com>
2020-12-29 23:33:55 +0900

    Math Class Lecture - 1

---
+++ Study/src/main/api/lang/etc/Test01.java
@@ -0,0 +1,14 @@
+package main.api.lang.etc;
+
+public class Test01 {
+    public static void main(String[] args) {
+
+        // java.lang.Math
+        //  - 생성자 X
+        //   : static 처리 or 생성을 제한
+
+        // abs() : 절대값
+        System.out.println(Math.abs(-1));
+    }
+
+}
