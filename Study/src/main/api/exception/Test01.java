b310f6677b9b22fa7b76608992e28975beee3dd7
Chany <inshakr2@naver.com>
2020-12-31 18:59:15 +0900

    Exception Lecture

---
+++ Study/src/main/api/exception/Test01.java
@@ -0,0 +1,8 @@
+package main.api.exception;
+
+public class Test01 {
+    public static void main(String[] args) {
+        // Error : 오류 (실행이 불가능한 상황)
+        // Exception : 예외 (실행 후에 문제가 되는 상황)
+    }
+}
