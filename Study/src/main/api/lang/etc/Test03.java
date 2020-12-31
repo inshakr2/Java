6184354e33103bd39e7ea5c533cda5c55b513e3e
Chany <inshakr2@naver.com>
2020-12-31 18:50:53 +0900

    Runtime Class Lecture - 1  && etc..

---
+++ Study/src/main/api/lang/etc/Test03.java
@@ -0,0 +1,15 @@
+package main.api.lang.etc;
+
+public class Test03 {
+    public static void main(String[] args) {
+
+        // java.lang.Runtime
+        //  - 생성자 X
+        //  - 사용자가 원하는 방식으로 생성하도록 유도,, = 생성 명령이 존재
+        Runtime run = Runtime.getRuntime();
+
+
+
+
+    }
+}
