301e64a68628053a11f9a15f0b823c11a7d911a9
Chany <inshakr2@naver.com>
2020-12-31 19:07:49 +0900

    Exception Lecture - intended exception

--- Study/src/main/api/exception/Test01.java
+++ Study/src/main/api/exception/Test01.java
@@ -1,8 +1,18 @@
 package main.api.exception;
 
+import java.util.Scanner;
+
 public class Test01 {
     public static void main(String[] args) {
         // Error : 오류 (실행이 불가능한 상황)
         // Exception : 예외 (실행 후에 문제가 되는 상황)
+
+        // 예외 유발
+        Scanner sc = new Scanner(System.in);
+        System.out.println("현재 나이를 입력하세요.");
+        int apple = sc.nextInt();
+        // int 이외의 값에서 예외가 발생한다
+
+
     }
 }
