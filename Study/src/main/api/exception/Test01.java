cadb94d77263e1b8e497af33ff9bdf4b2f1e4f37
Chany <inshakr2@naver.com>
2021-01-01 15:11:00 +0900

    Exception Lecture - Handling exception

--- Study/src/main/api/exception/Test01.java
+++ Study/src/main/api/exception/Test01.java
@@ -9,9 +9,19 @@ public class Test01 {
 
         // 예외 유발
         Scanner sc = new Scanner(System.in);
-        System.out.println("현재 나이를 입력하세요.");
+        System.out.println("현재 사과 개수를 입력하세요");
         int apple = sc.nextInt();
+        System.out.println("사람 수를 입력하세요");
+        int people = sc.nextInt();
+
+        System.out.println("한 사람당 먹을 수 있는 사과의 개수 = " + apple / people);
         // int 이외의 값에서 예외가 발생한다
+        // 수학적 계산에 오류가 발생한다.
+
+        // 예외는 2가지가 발생.
+        // - java.util.InputMismatchException
+        // - java.lang.ArithmeticException
+
 
 
     }
---
+++ Study/src/main/api/exception/Test02.java
@@ -0,0 +1,31 @@
+package main.api.exception;
+
+import java.util.Scanner;
+
+public class Test02 {
+    public static void main(String[] args) {
+        // 예외 처리 구문
+        // try : 감시블록
+        // catch : 처리블록
+
+        try {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("현재 사과 개수를 입력하세요");
+            int apple = sc.nextInt();   // InputMismatch
+            System.out.println("사람 수를 입력하세요");
+            int people = sc.nextInt();  // InputMismatch
+
+            System.out.println("한 사람당 먹을 수 있는 사과의 개수 = " + apple / people); // Arithmetic
+        } catch (ArithmeticException e1) {
+            // ArithmeticException이 발생하면 정보를 e1이라는 객체로 전달받음
+            System.out.println("인원수는 1이상 입력.");
+        }
+
+
+
+
+
+
+
+    }
+}
