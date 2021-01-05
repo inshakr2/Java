523b41a2a0a293c82d8b45d426cf1ad9f803a43b
Chany <inshakr2@naver.com>
2021-01-05 23:07:47 +0900

    java.util.Date API Lecture - start

---
+++ Study/src/main/api/util/date/Test01.java
@@ -0,0 +1,16 @@
+package main.api.util.date;
+
+import java.sql.SQLOutput;
+import java.util.Date;
+
+public class Test01 {
+    public static void main(String[] args) {
+        // java.util.Data
+        //  - 생성자 Deprecated가 많다 : Y2K 때문
+
+        Date a = new Date();
+
+        System.out.println(a);
+    }
+
+}
