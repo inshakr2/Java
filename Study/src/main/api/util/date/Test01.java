95a68972e4b3bd2c8ab474b7a7f262e0ea027a1e
Chany <inshakr2@naver.com>
2021-01-05 23:11:56 +0900

    java.util.Date API Lecture - Depreciate Constructor

--- Study/src/main/api/util/date/Test01.java
+++ Study/src/main/api/util/date/Test01.java
@@ -9,8 +9,16 @@ public class Test01 {
         //  - 생성자 Deprecated가 많다 : Y2K 때문
 
         Date a = new Date();
-
         System.out.println(a);
+
+        // 연월일을 설정하는 생성자 (Depreciate)
+        Date b = new Date(2020, 10, 23);
+        // 경고 + 표식이 나온다.
+        // 여러 문제점으로 앞으로 쓰지 말것을 권장 - 사라질 수 있다.
+
+        System.out.println(b); // Tue Nov 23 00:00:00 KST 3920
+        // 문제점 : 시간이 한국인 정서에 맞지 않게 나온다.
+        // 해결책 : 시간 형식을 설정하는 클래스를 찾아서 사용
     }
 
 }
