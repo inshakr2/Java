1208b1d54b4dca5b992ce3daff4176455b4f9b3a
Chany <inshakr2@naver.com>
2021-01-09 19:05:29 +0900

    Collection api Lecture - 1

---
+++ Study/src/main/api/collection/Test01.java
@@ -0,0 +1,20 @@
+package main.api.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Test01 {
+    public static void main(String[] args) {
+        // 프레임워크 : 강력한 규칙으로 사용자가 그대로 이용할 수 밖에 없도록 만들어진 도구
+        // 자바 컬렉션 프레임워크 : 자바에서 대용량 데이터를 관리하기 위해 만들어진 프레임워크
+
+        // 전화번호부 형식 저장소
+        Set a = new TreeSet();
+
+        // 코로나 명부 형식의 저장소
+        List b = new ArrayList();
+
+    }
+}
