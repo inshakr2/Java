cc03300baec733b112cc4d595346fdf52c61e3cc
Chany <inshakr2@naver.com>
2021-01-11 23:54:09 +0900

    Collection api Lecture - ArrayList / LinkedList

---
+++ Study/src/main/api/collection/Test02.java
@@ -0,0 +1,22 @@
+package main.api.collection;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Test02 {
+    public static void main(String[] args) {
+        // List : 순서가 유지되는 대용량 저장소
+        //  - ArrayList : 배열 형태로 붙어있는 저장소
+        //  - LinkedList : 연결 형태로 떨어져 있는 저장소
+
+        // ArrayList
+        ArrayList a = new ArrayList();
+        List b = new ArrayList();
+
+        LinkedList c = new LinkedList();
+        List d = new LinkedList();
+
+
+    }
+}
