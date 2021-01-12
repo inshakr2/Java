97acd792550977ff2f3031e78ac4a6b6379fce44
Chany <inshakr2@naver.com>
2021-01-12 20:20:30 +0900

    Collection api Lecture - new example (DeDuplicate)

--- Study/src/main/api/collection/Test04.java
+++ Study/src/main/api/collection/Test04.java
@@ -11,9 +11,14 @@ public class Test04 {
         Random r = new Random();
         for (int i = 0; i < 6; i++) {
             int n = r.nextInt(45) + 1;
-            lotto.add(n);
-        }
 
+            if(!lotto.contains(n)){
+                lotto.add(n);
+            } else {
+                System.out.println("중복");
+                i--;
+            }
+        }
 
         for (int n : lotto) {
             System.out.println("번호 : " + n);
