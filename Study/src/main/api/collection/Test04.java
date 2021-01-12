70430c66074209e07db9e6ac7810d76b18e7c108
Chany <inshakr2@naver.com>
2021-01-12 20:09:23 +0900

    Collection api Lecture - new example (Linked)

---
+++ Study/src/main/api/collection/Test04.java
@@ -0,0 +1,22 @@
+package main.api.collection;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
+public class Test04 {
+    public static void main(String[] args) {
+        // 로또 번호 6개 저장
+        List<Integer> lotto = new LinkedList<>();
+        Random r = new Random();
+        for (int i = 0; i < 6; i++) {
+            int n = r.nextInt(45) + 1;
+            lotto.add(n);
+        }
+
+
+        for (int n : lotto) {
+            System.out.println("번호 : " + n);
+        }
+    }
+}
