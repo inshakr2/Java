92804f262ab283ce23ab1affe6a027458ffb38b0
Chany <inshakr2@naver.com>
2021-01-14 23:30:38 +0900

    Collection api Lecture - Comparator

--- Study/src/main/api/collection/Test10.java
+++ Study/src/main/api/collection/Test10.java
@@ -22,11 +22,17 @@ public class Test10 {
         // List를 내가 원하는 방식으로 정렬하는 방식 (Custom sort)
         // 내림차순처럼 별도의 정렬방식을 구현하려면 정렬에 해당하는 객체를 생성
         // Comparable , Comparator
-
+        // Comparator라는 인터페이스를 익명중첩클래스 형태로 생성
+        // return 값에 따른 정렬
+        // 0 : 현상유지(통과)     양수 : 두 자리를 Change       음수 : 두자리 유지
         Comparator<Integer> c = new Comparator<Integer>() {
             @Override
-            public int compare(Integer o1, Integer o2) {
-                return 0;
+            public int compare(Integer x, Integer y) {
+                if (y > x) {
+                    return 1;
+                } else {
+                    return -1;
+                }
             }
         };
         Collections.sort(list, c);
