6df85f8ef959b41e9617f1c38eac2b2f6a6e20f0
Chany <inshakr2@naver.com>
2021-01-16 15:19:15 +0900

    Collection api Lecture - compareTo

---
+++ Study/src/main/api/collection/Test11.java
@@ -0,0 +1,14 @@
+package main.api.collection;
+
+public class Test11 {
+    public static void main(String[] args) {
+        // .compareTo()
+        String a = "김철수";
+        String b = "이민수";
+        System.out.println(a.compareTo(b));
+        System.out.println(b.compareTo(a));
+
+        System.out.println('김' - '이');
+
+    }
+}
