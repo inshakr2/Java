83f6ebe359f819a12a31991b6d8630efbece0f89
Chany <inshakr2@naver.com>
2021-01-16 15:27:06 +0900

    Collection api Lecture - sort String list

---
+++ Study/src/main/api/collection/Test11_1.java
@@ -0,0 +1,34 @@
+package main.api.collection;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Test11_1 {
+    public static void main(String[] args) {
+
+        List<String> list = new ArrayList<>();
+        list.add("자바");
+        list.add("파이썬");
+        list.add("C언어");
+        list.add("자바스크립트");
+        list.add("루비");
+
+        // 내림차순으로 하려면,, 비효율적
+//        Collections.sort(list);
+//        Collections.reverse(list);
+//        System.out.println(list);
+
+        Comparator<String> c = new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o2.compareTo(o1);
+            }
+        };
+
+        Collections.sort(list, c);
+        System.out.println(list);
+
+    }
+}
