606694610e63ee8aeab0b7974cb6d2c5cca743a6
Chany <inshakr2@naver.com>
2021-01-16 16:24:20 +0900

    Collection api Lecture - object list

--- Study/src/main/api/collection2/Test01.java
+++ Study/src/main/api/collection2/Test01.java
@@ -1,4 +1,24 @@
 package main.api.collection2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Test01 {
+    public static void main(String[] args) {
+        // 목표 : Student를 저장하는 list 구현
+
+        List<Student> students = new ArrayList<>();
+
+        students.add(new Student("홍길동", 100));
+        students.add(new Student("파이리", 90));
+        students.add(new Student("손흥민", 100));
+
+        // size
+        System.out.println(students);
+        System.out.println(students.size());
+
+        // 출력
+        System.out.println(students.get(0).getName());
+
+    }
 }
