ac6bbb05d694a8ad88c3656c9ab73e7f093deb0d
Chany <inshakr2@naver.com>
2021-01-23 00:44:28 +0900

    Collection api Lecture - Set (operation)

---
+++ Study/src/main/api/collection3/Test03.java
@@ -0,0 +1,28 @@
+package main.api.collection3;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Test03 {
+    public static void main(String[] args) {
+        // Set은 집합연산에 특화되어 있다.
+        // 합집합 , 차집합 , 교집합
+
+        Set<Integer> a = new TreeSet<>();
+        a.add(10);
+        a.add(20);
+        a.add(30);
+
+        Set<Integer> b = new TreeSet<>();
+        b.add(20);
+        b.add(30);
+        b.add(40);
+        b.add(50);
+
+        // 합집합
+        Set<Integer> c = new TreeSet<>();
+        c.addAll(a);
+        c.addAll(b);
+        System.out.println("c = " + c);
+    }
+}
