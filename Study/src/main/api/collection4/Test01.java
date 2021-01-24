cbf24fd6659a6a68c476ebcf72c3db7c81f21c4c
Chany <inshakr2@naver.com>
2021-01-24 23:37:34 +0900

    Collection api Lecture - Map concept

---
+++ Study/src/main/api/collection4/Test01.java
@@ -0,0 +1,11 @@
+package main.api.collection4;
+
+public class Test01 {
+    public static void main(String[] args) {
+        // Map(k,v)
+        // - 두 개의 데이터를 세트로 저장하는 저장소
+        // - K : key, 이름 역할을 수행하며 중복이 불가능
+        // - V : value, 값의 역할을 수행하며 중복 가능
+
+    }
+}
