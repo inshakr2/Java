227b452b393f0e5c4cea7807ea89af4e11eb3f7f
Chany <inshakr2@naver.com>
2021-01-24 23:39:41 +0900

    Collection api Lecture - Map basic

--- Study/src/main/api/collection4/Test01.java
+++ Study/src/main/api/collection4/Test01.java
@@ -1,11 +1,22 @@
 package main.api.collection4;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Test01 {
     public static void main(String[] args) {
         // Map(k,v)
         // - 두 개의 데이터를 세트로 저장하는 저장소
         // - K : key, 이름 역할을 수행하며 중복이 불가능
         // - V : value, 값의 역할을 수행하며 중복 가능
+
+        // 저장소 생성
+        Map<String, Integer> map = new HashMap<>();
+
+        // 데이터 추가
+        map.put("a", 20);
+
+        System.out.println(map.get("a"));
 
     }
 }
